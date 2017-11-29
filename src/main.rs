0a81def57ac7a1b1546dbb088c9bfbfe5ebf9784
Frederic de Zorzi <f@idez.net>
2017-11-29 14:12:40 +0200

    ok test

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,7 @@ use regex::Regex;
 fn filter_dump(include_tables: Option<String>, exclude_tables: Option<String>, copy_only: bool,
                truncate: bool) {
     let stdin = io::stdin();
-    let mut stdin = stdin.lock();
     let copy_re = Regex::new(r"^COPY (\w+) \(([\w, ]+)\) FROM stdin;").unwrap();
-    let mut line = String::new();
     
     let mut in_copy_statement = false;
     let mut output_copy = false;
@@ -24,41 +22,45 @@ fn filter_dump(include_tables: Option<String>, exclude_tables: Option<String>, c
         Some(re) => Regex::new(&re).unwrap(),
         None => Regex::new("0").unwrap()
     };
-    
-    while stdin.read_line(&mut line).unwrap() > 0 {
-        if in_copy_statement {
-            if line == "\\.\n" {
-                in_copy_statement = false;
-            }
-            if output_copy {
-                print!("{}", line)
-            }
-        }
-        else {
-            match copy_re.captures(&line) {
-                None => {
-                    if !copy_only {
-                        print!("{}", line);
+
+    for line in stdin.lock().lines() {
+        match line {
+            Ok(line) => {
+                if in_copy_statement {
+                    if line == "\\." {
+                        in_copy_statement = false;
                     }
-                },
-                Some(caps) => {
-                    let current_table = String::from(&caps[1]);
-                    if include_tables_re.is_match(&current_table) &&
-                        !exclude_tables_re.is_match(&current_table) {
-                        output_copy = true;
-                        if truncate {
-                            println!("TRUNCATE table {};", current_table);
-                        }
-                        print!("{}", line);
+                    if output_copy {
+                        println!("{}", line)
                     }
-                    else {
-                        output_copy = false;
+                }
+                else {
+                    match copy_re.captures(&line) {
+                        None => {
+                            if !copy_only {
+                                println!("{}", line);
+                            }
+                        },
+                        Some(caps) => {
+                            let current_table = String::from(&caps[1]);
+                            if include_tables_re.is_match(&current_table) &&
+                                !exclude_tables_re.is_match(&current_table) {
+                                    output_copy = true;
+                                    if truncate {
+                                        println!("TRUNCATE table {};", current_table);
+                                    }
+                                    println!("{}", line);
+                                }
+                            else {
+                                output_copy = false;
+                            };
+                            in_copy_statement = true;
+                        },
                     };
-                    in_copy_statement = true;
-                },
-            };
+                }
+            },
+            Err(_) => continue,
         }
-        line.clear();
     }
 }
 
@@ -72,14 +74,17 @@ fn main() {
     let program = args[0].clone();
 
     let mut opts = Options::new();
-    opts.optopt("t", "table", "output only tables matching", "NAME");
-    opts.optopt("T", "exclude-table", "no not output tables matching", "NAME");
+    opts.optopt("t", "table", "output only tables matching NAME", "NAME");
+    opts.optopt("T", "exclude-table", "no not output tables matching NAME", "NAME");
     opts.optflag("c", "copy-only", "output only COPY FROM statements");
     opts.optflag("r", "truncate", "empty tables before COPY FROM statements");
     opts.optflag("h", "help", "print this help menu");
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m }
-        Err(f) => { panic!(f.to_string()) }
+        Err(f) => {
+            print_usage(&program, opts);
+            panic!(f.to_string())
+        }
     };
     if matches.opt_present("h") {
         print_usage(&program, opts);
